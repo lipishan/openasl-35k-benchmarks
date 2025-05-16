3cda0339286822e0f96f121ab906ecc315a42e69
lipishan <lipishan@buffalo.edu>
2025-05-16 02:57:25 -0400

    First commit

---
+++ run_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python -m train.train_mdm  \
+    --save_dir save/openasl.test.05-13 \
+    --dataset openasl \
+    --cond_mask_prob 0 \
+    --lambda_rcxyz 0 \
+    --lambda_vel 1 \
+    --lambda_fc 0 \
+    --batch_size 128 \
+    --gen_guidance_param 1 \
+    --eval_during_training \
+    --overwrite 
+
+
+
